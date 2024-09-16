634511711b1ecae9f2f79f766f906044e466fa9d
hakoobyannn <ovllvlvvvla@gmail.com>
2024-09-16 18:12:06 +0300

    project first commit

---
+++ sqript.js
@@ -0,0 +1,26 @@
+// function tiv (n){
+//     let x = 0
+//     for(let i=1;i<=100;i++){
+//         if (i% n== 2){
+//             x+=i;
+//         }
+//     }
+//     return x
+// }
+// const n = 7;
+// console.log("Gumary",tiv(n));
+
+/**function tiv(n){
+    let x = 1
+    for(let i=100;i<=1000;i++){
+        if(i%n==3){
+            x*=i
+    }
+}
+return x
+}
+console.log(tiv(n));
+/** */
+ 
+
+
